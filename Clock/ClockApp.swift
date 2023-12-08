1514ab405ac67cbd8bfd7da8accc7a19dbe8c34d
bmoxey <bmoxey@gmail.com>
2023-12-08 18:14:18 +1100

    Initial Commit

---
+++ Clock/ClockApp.swift
@@ -0,0 +1,17 @@
+//
+//  ClockApp.swift
+//  Clock
+//
+//  Created by Brett Moxey on 8/12/2023.
+//
+
+import SwiftUI
+
+@main
+struct ClockApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
